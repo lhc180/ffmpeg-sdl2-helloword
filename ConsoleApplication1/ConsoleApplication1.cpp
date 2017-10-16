2afb5a78bc00b0210b1b1ae658a505ba83cc5198
肖兴宇 <1577915064@qq.com>
2017-10-16 14:55:48 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+// ConsoleApplication1.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdio.h>  
+#include "sdl2/sdl.h"
+//#include "sdl2/sdl_main.h"
+#define __STDC_CONSTANT_MACROS  
+
+#ifdef _WIN32  
+//Windows  
+extern "C"
+{
+#include "libavcodec/avcodec.h"  
+#include "libavformat/avformat.h"  
+#include "libavfilter/avfilter.h"  
+};
+#else  
+//Linux...  
+#ifdef __cplusplus  
+extern "C"
+{
+#endif  
+#include <libavcodec/avcodec.h>  
+#include <libavformat/avformat.h>  
+#include <libavfilter/avfilter.h>  
+#ifdef __cplusplus  
+};
+#endif  
+#endif  
+
+
+int main(int argc, char* argv[])
+{
+	av_register_all();//ffmpeg初始化
+	printf("%s\n", avcodec_configuration());//输出ffmpeg编码信息
+	SDL_Init(SDL_INIT_EVERYTHING);
+	auto win=SDL_CreateWindow("helloworld",0,0,500,500,SDL_WINDOW_SHOWN);//创建窗口
+	auto  render= SDL_CreateRenderer(win,-1, SDL_RENDERER_SOFTWARE);//创建渲染器
+	SDL_SetRenderDrawColor(render, 0, 255, 0, 255);//渲染绿色
+	SDL_RenderClear(render);
+	SDL_RenderPresent(render);//渲染
+	SDL_Delay(2000);
+	SDL_Quit();
+    return 0;
+}
+
